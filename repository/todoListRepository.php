6d2ac7ca2e08462a6a91fc94eca30b8e937901b7
Restu Gede Purnama <118859039+erstuu@users.noreply.github.com>
2024-01-31 16:37:59 +0700

    Menambahkan repository

---
+++ repository/todoListRepository.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Repository 
+{
+    use Entity\TodoList;
+
+    interface TodoListRepository 
+    {
+        function save(ToDoList $todoList): void;
+        function remove(int $number): bool;
+        function findAll(): array;
+    }
+
+    class TodoListRepositoryImpl implements TodoListRepository
+    {
+        public array $todolist = array();
+
+        function save(TodoList $todoList): void 
+        {
+            $number = sizeof($this->todolist) + 1;
+            $this->todolist[$number] = $todoList;
+        }
+
+        function remove(int $number): bool
+        {
+            if($number > sizeOf($this->todolist)) {
+                return false;
+            }
+        
+            for($i = $number; $i < sizeof($this->todolist); $i++) {
+                $this->todolist[$i] = $this->todolist[$i + 1];
+            }
+        
+            unset($this->todolist[sizeof($this->todolist)]);
+
+            return true;
+        }
+
+        function findAll(): array
+        {
+            return $this->todolist;
+        }
+    }
+}
